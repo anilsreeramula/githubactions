241b90b3d495acbccf0bd74cdf51c6fd1562aec6
anilsreeramula <sak.n0028@gmail.com>
2026-06-26 14:02:26 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "ca-central-1"
+}
+
+resource "aws_instance" "ec2" {
+    ami = "ami-0d19d7a9bc91b3550"
+    instance_type = "t3.micro"
+    tags = {
+      Name = "ec2_githubactions"
+    }
+}
