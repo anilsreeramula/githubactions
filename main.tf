b565ecf9946f726e2901b1d8be1120f2803dc5ee
anilsreeramula <sak.n0028@gmail.com>
2026-06-26 19:46:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ provider "aws" {
 
 resource "aws_instance" "ec2" {
     ami = "ami-0d19d7a9bc91b3550"
-    instance_type = "t3.micro"
+    instance_type = "t3.small"
     tags = {
-      Name = "ec2_githubactions"
+      Name = "server_withoutkeys"
     }
 }
